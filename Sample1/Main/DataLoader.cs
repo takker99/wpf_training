e5ec9b5f3ba8061a30b0c40e8c3cfa54c8e4af44
takker99 <electronic.homuhomu@gmail.com>
2020-04-03 17:40:11 +0900

    :construction: データを表現するクラスを作成し始めた

---
+++ Sample1/Main/DataLoader.cs
@@ -0,0 +1,11 @@
+namespace Sample1
+{
+    public static class DataLoader
+    {
+        private static AppData _createNewTestData()
+        {
+            var appData=new AppData();
+        }
+    }
+
+}
