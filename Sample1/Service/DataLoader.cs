5a36e538bd6b57678a0a9272084263a45173fd0b
takker99 <electronic.homuhomu@gmail.com>
2020-04-04 16:16:20 +0900

    Revert "fdfd"
    This reverts commit a626f170ff6856bb3d14f40be2807b0b202871c9.

--- Sample1/Service/DataLoader.cs
+++ Sample1/Service/DataLoader.cs
@@ -1,3 +1,4 @@
+using Sample1.Model
 namespace Sample1
 {
     // Service 層に位置するクラス
@@ -5,15 +6,15 @@ namespace Sample1
     {
         /// <summary>新規テストデータを作成します。</summary>
         /// <returns>新規テストデータを表すAppData。</returns>
-        private static Model.AppData _createNewTestData()
+        private static AppData _createNewTestData()
         {
-            var appData = new Model.AppData();
+            var appData = new AppData();
             appData.Student.Name = "新しい生徒";
             appData.Student.ClassNumber = "所属クラス";
             appData.Student.Sex = "女";
 
-            appData.Physicals.Add(new Model.PhysicalInformation { Id = 1 });
-            appData.TestPoints.Add(new Model.TestPointInformation
+            appData.Physicals.Add(new PhysicalInformation { Id = 1 });
+            appData.TestPoints.Add(new TestPointInformation
             {
                 Id = 1,
                 TestDate = "新しい試験日",
@@ -25,12 +26,12 @@ namespace Sample1
         /// <summary>データファイルからロードします。</summary>
         /// <param name="dataFilePath">データファイルのフルパスを表す文字列。</param>
         /// <returns>データファイルからロードしたAppData。</returns>
-        private static Model.AppData loadFromFile(string dataFilePath) => new Model.AppData();
+        private static AppData loadFromFile(string dataFilePath) => new AppData();
 
         /// <summary>データをロードします。</summary>
         /// <param name="dataFilePath">データファイルのフルパスを表す文字列。</param>
         /// <returns>ロードしたデータを表すAppData。</returns>
-        public static AppData.Model Load(string dataFilePath)
+        public static AppData Load(string dataFilePath)
         {
             if (dataFilePath == string.Empty) { return DataLoader.createNewTestData(); }
             else { return DataLoader.loadFromFile(dataFilePath); }
