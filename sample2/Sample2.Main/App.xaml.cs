715263d555f0a9bc0e0016c32524bfc5e937b133
takker99 <electronic.homuhomu@gmail.com>
2020-04-20 05:26:01 +0900

    :sparkles: (MainWindow) connect view and viewmodel

--- sample2/Sample2.Main/App.xaml.cs
+++ sample2/Sample2.Main/App.xaml.cs
@@ -4,7 +4,6 @@ using Prism.Unity;
 using System;
 using System.Reflection;
 using System.Windows;
-using Sample2.Main;
 
 namespace Sample2
 {
@@ -14,24 +13,16 @@ namespace Sample2
     public partial class App : PrismApplication
     {
         protected override Window CreateShell()
-            => this.Container.Resolve<MainWindow>();
+            => this.Container.Resolve<Views.MainWindow>();
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry) { }
 
-        // "...Views.hogehoge.xaml" という View の View Model を "...ViewModels.hogehoge.cs" に自動で設定する
+        // 手動でView - ViewModel を関連付ける
         protected override void ConfigureViewModelLocator()
         {
             base.ConfigureViewModelLocator();
 
-            ViewModelLocationProvider.
-                SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
-            {
-                // "...Sample2.Views.MainWindow" を "...Sample2.ViewModels.MainWindow" に置き換える
-                string viewName = viewType.FullName.Replace(".Views.",".ViewModels.");
-                string viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
-                string viewModelName = $"{viewName}, {viewAssemblyName}";
-                return Type.GetType(viewModelName);
-            });
+            ViewModelLocationProvider.Register<Views.MainWindow, ViewModels.MainWindow>();
         }
     }
 }
