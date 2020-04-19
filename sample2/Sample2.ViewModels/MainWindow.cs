fed243dc1b9f32ed4559955e16c5003ec1132b65
takker99 <electronic.homuhomu@gmail.com>
2020-04-20 06:33:47 +0900

    :fire: unable to show text
    OperandとAnswerを表示するために、いらないtext blockを削除した

--- sample2/Sample2.ViewModels/MainWindow.cs
+++ sample2/Sample2.ViewModels/MainWindow.cs
@@ -3,8 +3,5 @@ using Reactive.Bindings;
 
 namespace Sample2.ViewModels
 {
-    public class MainWindow : BindableBase
-    {
-        public ReactivePropertySlim<string> Text { get; } = new ReactivePropertySlim<string>("Hello, Prism!");
-    }
+    public class MainWindow : BindableBase { }
 }
