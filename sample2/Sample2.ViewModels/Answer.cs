495a093e61a3ffb986850712c753baab40186940
takker99 <electronic.homuhomu@gmail.com>
2020-04-20 09:16:37 +0900

    :sparkles: TextBoxに入力された値をリアルタイムに計算・表示する
    入力エラー検出もできる。

--- sample2/Sample2.ViewModels/Answer.cs
+++ sample2/Sample2.ViewModels/Answer.cs
@@ -1,10 +1,30 @@
+using Prism.Events;
 using Prism.Mvvm;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace Sample2.ViewModels
 {
-    public class Answer : BindableBase
+    public class Answer : BindableBase,System.IDisposable
     {
-        public ReactivePropertySlim<string> Text { get; } = new ReactivePropertySlim<string>("4");
+        public ReactivePropertySlim<string> Text { get; }
+
+        public Answer(IEventAggregator eventAggregator)
+        {
+            this.Text = new ReactivePropertySlim<string>("4").AddTo(this._disposables);
+
+            eventAggregator.
+               GetEvent<PubSubEvent<double>>()
+               .Subscribe(operand =>
+               {
+                   this.Text.Value = (operand * operand).ToString();
+               },true)
+               .AddTo(this._disposables);
+        }
+
+        void System.IDisposable.Dispose() => this._disposables.Dispose();
+
+        // ÉSÉ~î†
+        private readonly System.Reactive.Disposables.CompositeDisposable _disposables = new System.Reactive.Disposables.CompositeDisposable();
     }
 }
--- sample2/Sample2.ViewModels/Operand.cs
+++ sample2/Sample2.ViewModels/Operand.cs
@@ -1,24 +1,57 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Reactive.Linq;
+
+using Prism.Events;
 using Prism.Mvvm;
+
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace Sample2.ViewModels
 {
-    public class Operand : BindableBase
+    public class Operand : BindableBase, System.IDisposable
     {
+        // Validation機能を使うので、ReactivePropertySlimは使えない
+        // Viewから値を変更できるようにするので、ReadOnlyReactivePropertyも使わない
         [Required, Range(-10000,10000)]
         public ReactiveProperty<string> Text { get; }
 
-        public Operand()
+        public Operand(IEventAggregator eventAggregator)
         {
             // SetValidateAttribute:
             //
             // エラー検出を可能にする。検出条件式は
             // System.ComponentModel.DataAnnotations属性で指定する
-            this.Text = new ReactiveProperty<string>("2").
-                SetValidateAttribute(() => this.Text);
+            this.Text = new ReactiveProperty<string>("2")
+                .SetValidateAttribute(() => this.Text)
+                .AddTo(this._disposables);
 
+            // ViewModel間でデータをやり取りするためにEventAggregatorを使う
+            Observable
+                .WithLatestFrom
+                (
+                    // 入力されたテキストと、直前に発行された入力値の
+                    // エラー検出結果を一つにしてストリームに流す
+                    this.Text,
+                    this.Text.ObserveHasErrors,
+                    (text, error) => (text, error)
+                )
+                // 入力値が正常なもののみ通す
+                .Where(z => !z.error)
+                // 数値に変換して、EventAggregatorで発行する。
+                .Subscribe(z =>
+                {
+                    eventAggregator.GetEvent<PubSubEvent<double>>()
+                                   .Publish(Double.Parse(z.text));
+                })
+                // ゴミ箱を指定
+                .AddTo(this._disposables);
         }
+
+        void System.IDisposable.Dispose() => this._disposables.Dispose();
+
+        // observableのゴミ箱
+        private readonly System.Reactive.Disposables.CompositeDisposable _disposables = new System.Reactive.Disposables.CompositeDisposable();
     }
 }
