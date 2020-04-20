06dd71952274a4b85c4d4ce0629572033085f7ee
takker99 <electronic.homuhomu@gmail.com>
2020-04-21 08:47:18 +0900

    :sparkles: append all in the text box to temp.txt
    - implement the save command
    - 現状ではファイルの読み込みができない。今後実装する

--- sample3/StartUp/ViewModels/MainWindow.cs
+++ sample3/StartUp/ViewModels/MainWindow.cs
@@ -1,21 +1,37 @@
 using Prism.Mvvm;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace Sample3.StartUp.ViewModels
 {
     public class MainWindow : BindableBase
     {
-        public ReactivePropertySlim<string> Text;
-        public ReactiveCommand SaveTextCommand;
+        // null合体代入演算子を用いた以下の記法については次を参照：
+        // - https://qiita.com/okazuki/items/2f0832133eac4427b72c
 
-        public MainWindow()
+        public ReactivePropertySlim<string> Text { get; }
+        public AsyncReactiveCommand<object> SaveTextCommand { get; }
+
+        public MainWindow(Models.IStreamManager streamManager)
         {
+            // DI container から modelを受け取る
+            this._streamManager = streamManager;
+
             this.Text = new ReactivePropertySlim<string>("Write any text here!")
+            .AddTo(this._disposable);
+            //this.Text=new ReactivePropertySlim<string>(await this._streamManager.ReadTextAsync())
+
+            this.SaveTextCommand = new AsyncReactiveCommand()
+                .WithSubscribe(async _ => await this._streamManager.WriteTextAsync(this.Text.Value).ConfigureAwait(false))
                 .AddTo(this._disposable);
+
         }
 
         private System.Reactive.Disposables.CompositeDisposable _disposable 
             = new System.Reactive.Disposables.CompositeDisposable();
+
+        private Models.IStreamManager _streamManager;
     }
 }
