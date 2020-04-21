9847e944f83930c96715944e3054434b4aa46b9e
takker99 <electronic.homuhomu@gmail.com>
2020-04-21 16:02:12 +0900

    :sparkles: add view and viewmodel
    すべて実装したはずなのに、なぜかエラーが発生する。
    ViewとViewmodelの関連付けで何故かエラーが出る。
    原因は全くわからない

--- sample4/StartUp/App.xaml.cs
+++ sample4/StartUp/App.xaml.cs
@@ -1,12 +1,11 @@
-﻿using Prism.Ioc;
+using Prism.Ioc;
 using Prism.Mvvm;
 using Prism.Unity;
 using System;
 using System.Reflection;
 using System.Windows;
-using Sample4.StartUp.Views;
 
-namespace Sample4.StartUp
+namespace Sample4
 {
     /// <summary>
     /// Interaction logic for App.xaml
@@ -14,24 +13,26 @@ namespace Sample4.StartUp
     public partial class App : PrismApplication
     {
         protected override Window CreateShell()
-            => this.Container.Resolve<Views.MainWindow>();
+            => this.Container.Resolve<StartUp.Views.MainWindow>();
 
-        protected override void RegisterTypes(IContainerRegistry containerRegistry) { }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+            => containerRegistry.Register<Models.IHeavyWorker, Models.IHeavyWorker>();
 
         // "...Views.hogehoge.xaml" という View の View Model を "...ViewModels.hogehoge.cs" に自動で設定する
         protected override void ConfigureViewModelLocator()
         {
             base.ConfigureViewModelLocator();
 
-            ViewModelLocationProvider.
-                SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
-            {
-                // "...Sample4.StartUp.Views.MainWindow" を "...Sample4.StartUp.ViewModels.MainWindow" に置き換える
-                string viewName = viewType.FullName.Replace(".Views.",".ViewModels.");
-                string viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
-                string viewModelName = $"{viewName}, {viewAssemblyName}";
-                return Type.GetType(viewModelName);
-            });
+            ViewModelLocationProvider.Register<StartUp.Views.MainWindow, StartUp.ViewModels.MainWindow>();
+            //ViewModelLocationProvider.
+            //    SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            //{
+            //    // "...Sample4.StartUp.Views.MainWindow" を "...Sample4.StartUp.ViewModels.MainWindow" に置き換える
+            //    string viewName = viewType.FullName.Replace(".Views.",".ViewModels.");
+            //    string viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+            //    string viewModelName = $"{viewName}, {viewAssemblyName}";
+            //    return Type.GetType(viewModelName);
+            //});
         }
     }
 }
--- sample4/StartUp/ViewModels/MainWindow.cs
+++ sample4/StartUp/ViewModels/MainWindow.cs
@@ -1,13 +1,54 @@
+using System;
 using Prism.Mvvm;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace Sample4.StartUp.ViewModels
 {
     public class MainWindow : BindableBase
     {
-        public MainWindow()
+        public ReactivePropertySlim<double> ProgressValue { get; }
+        public ReactivePropertySlim<string> Message { get; }
+        /// <summary>
+        /// true:  処理を実行可能
+        /// false: 処理を実行中
+        /// </summary>
+        public ReactivePropertySlim<bool> CanHeavyWork { get; }
+        public AsyncReactiveCommand RunHeavyWorkCommand { get; }
+        public ReactiveCommand CancelHeavyWorkCommand { get; }
+        public MainWindow(Models.IHeavyWorker heavyWorker)
         {
+            // DI containerからmodelを受け取る
+            this._heavyWorker = heavyWorker;
 
+            // ReactivePropertyの初期化
+            this.ProgressValue = new ReactivePropertySlim<double>(0.0).AddTo(this._disposables);
+            this.Message = new ReactivePropertySlim<string>("").AddTo(this._disposables);
+            this.CanHeavyWork = new ReactivePropertySlim<bool>(true).AddTo(this._disposables); // 初期状態では実行可能
+
+            this.RunHeavyWorkCommand
+                = this.CanHeavyWork // 処理が実行可能ならば
+                .ToAsyncReactiveCommand() // 処理を非同期で開始する
+                .WithSubscribe(async () => await this._heavyWorker.HeavyWork(this._progressInfo))
+                .AddTo(this._disposables);
+
+            this.CancelHeavyWorkCommand
+                = this.CanHeavyWork
+                .Inverse() // 処理を実行中であれば
+                .ToReactiveCommand() // 処理を中断する
+                .WithSubscribe(() => this._heavyWorker.Cancel())
+                .AddTo(this._disposables);
+
+            this._progressInfo = new Progress<Models.ProgressInfo>(e =>
+              {
+                  this.ProgressValue.Value = e.Value;
+                  this.Message.Value = e.Message;
+              });
         }
+
+        private readonly Models.IHeavyWorker _heavyWorker;
+        private readonly Progress<Models.ProgressInfo> _progressInfo;
+        private System.Reactive.Disposables.CompositeDisposable _disposables 
+            = new System.Reactive.Disposables.CompositeDisposable(); // ゴミ箱
     }
 }
