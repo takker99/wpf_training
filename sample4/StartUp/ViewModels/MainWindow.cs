2cd59d4002b7d0b7fcf424a0d60193b7175beed7
takker99 <electronic.homuhomu@gmail.com>
2020-04-21 14:05:01 +0900

    :truck: make the directory structure for Prism

---
+++ sample4/StartUp/ViewModels/MainWindow.cs
@@ -0,0 +1,13 @@
+using Prism.Mvvm;
+using Reactive.Bindings;
+
+namespace Sample4.StartUp.ViewModels
+{
+    public class MainWindow : BindableBase
+    {
+        public MainWindow()
+        {
+
+        }
+    }
+}
