d29068d7d0ae623ed635456c85a61dfa12493ba5
takker99 <electronic.homuhomu@gmail.com>
2020-04-21 14:02:00 +0900

    :tada: add a new wpf application
    下記のappを作ってみる
    - http://nineworks2.blog.fc2.com/blog-entry-4.html

---
+++ sample4/StartUp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using System;
+using System.Reflection;
+using System.Windows;
+using Sample4.StartUp.Views;
+
+namespace Sample4.StartUp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+            => this.Container.Resolve<Views.MainWindow>();
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) { }
+
+        // "...Views.hogehoge.xaml" という View の View Model を "...ViewModels.hogehoge.cs" に自動で設定する
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.
+                SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                // "...Sample4.StartUp.Views.MainWindow" を "...Sample4.StartUp.ViewModels.MainWindow" に置き換える
+                string viewName = viewType.FullName.Replace(".Views.",".ViewModels.");
+                string viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                string viewModelName = $"{viewName}, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+        }
+    }
+}
