b8d955bddf99e98efacffb4b106bc551800d3845
takker99 <electronic.homuhomu@gmail.com>
2020-04-23 15:28:38 +0900

    :tada: make a new sample program using Dapper
    Dapperを用いたdatabase sample programを作成する
    このprogramから、どうやってタスク管理ツールを作成するかを
    学ぶ

---
+++ sample6/StartUp/ViewModels/MainWindow.cs
@@ -0,0 +1,14 @@
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace Sample3.StartUp.ViewModels
+{
+    public class MainWindow : BindableBase
+    {
+        public MainWindow()
+        {
+
+        }
+    }
+}
---
+++ sample6/StartUp/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace Sample3.StartUp.ViewModels
+{
+    public class MainWindow : BindableBase
+    {
+        public MainWindow()
+            => InitializedComponent();
+    }
+}
