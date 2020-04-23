85343aceb25dece9152324edf50574254c7ec087
takker99 <electronic.homuhomu@gmail.com>
2020-04-23 12:40:28 +0900

    :sparkles: Dispose ViewModel when MainWindow is closed
    LivetCask.Behaviorsを使用して、Window終了時に
    MainWindowのViewModelをDisposeするようにした。
    > Prism Modulesに含まれるViewsはこれではDisposeできない

--- Sample1/StartUp/ViewModels/MainWindow.cs
+++ Sample1/StartUp/ViewModels/MainWindow.cs
@@ -1,14 +1,37 @@
-﻿using Prism.Mvvm;
+using Prism.Mvvm;
 using Reactive.Bindings;
 
 namespace Sample1.StartUp.ViewModels
 {
-    public class MainWindow : BindableBase
+    public class MainWindow : BindableBase, System.IDisposable
     {
         public ReactivePropertySlim<string> Title { get; }
         public MainWindow()
         {
             this.Title = new ReactivePropertySlim<string>("Prism Application");
         }
+
+
+        // Windowを閉じるときにViewModelsをDisposeする
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposedValue)
+            {
+                if (disposing)
+                {
+                    this._disposables.Dispose();
+                }
+                this._disposedValue = true;
+            }
+        }
+
+        // このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+        public void Dispose()
+            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+            => this.Dispose(true);
+
+        private bool _disposedValue = false; // 重複する呼び出しを検出するには
+        private System.Reactive.Disposables.CompositeDisposable _disposables
+            = new System.Reactive.Disposables.CompositeDisposable();
     }
 }
