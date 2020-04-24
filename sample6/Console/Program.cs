f7f28d2de4689aa7f8a113f931984c3a80d58637
takker99 <electronic.homuhomu@gmail.com>
2020-04-24 18:34:33 +0900

    :sparkles: Add a console app to examin System.Text.Json
    DB設定情報fileをxmlからjsonに変えたかったので、使い方を調べるために
    簡単なconsole applicationを作成した。

---
+++ sample6/Console/Program.cs
@@ -0,0 +1,50 @@
+using System.Text.Json;
+using System.Text;
+using static System.Console;
+
+namespace Sample6.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, takker!");
+
+            // System.Text.Jsonのテストコード
+
+            var testData = new DBAccess.Connection.DbConnectInformation()
+            {
+                DataSource = @"{exePath}\SampleDb.sqlite3",
+                DbType = DBAccess.Connection.DatabaseType.SQLite,
+                Number = 0,
+            };
+
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true,
+            };
+            // JSONでobjectをserializeする
+            byte[] output = JsonSerializer.SerializeToUtf8Bytes(testData, jsonOptions);
+            WriteLine($"The result of serialization:\n{Encoding.UTF8.GetString(output)}");
+
+            // fileに保存する
+            string path = @"e:\json_test.json";
+            System.IO.File.WriteAllBytes(path, output);
+            WriteLine("Successfully serialized!");
+
+            // fileから読み込む
+            var utf8Reader = new Utf8JsonReader(output);
+            var testData2=JsonSerializer.Deserialize<DBAccess.Connection.DbConnectInformation>(ref utf8Reader, jsonOptions) ;
+
+            WriteLine("Successfully Desirialized!");
+            WriteLine("Result:");
+            WriteLine($"DataSource = {testData2.DataSource}");
+            WriteLine($"DbType = {testData2.DbType}");
+            WriteLine($"Number = {testData2.Number}");
+
+            WriteLine("Successfully finished!");
+        }
+    }
+}
