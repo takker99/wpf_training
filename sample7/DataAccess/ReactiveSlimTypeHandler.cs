1701f47de282e1d4068d4357a219662d0dc46e76
takker99 <electronic.homuhomu@gmail.com>
2020-04-25 11:23:40 +0900

    :recycle: 型判別処理をより確実な型switchに置き換えた

--- sample7/DataAccess/ReactiveSlimTypeHandler.cs
+++ sample7/DataAccess/ReactiveSlimTypeHandler.cs
@@ -2,7 +2,7 @@ using System.Data;
 using Reactive.Bindings;
 using static Dapper.SqlMapper;
 
-namespace Takker.DataAccess
+namespace Sample7.DataAccess
 {
 	/// <summary>ReactivePropertySlim<T>型用ハンドラを表します。</summary>
 	public class ReactiveSlimTypeHandler<T> : TypeHandler<ReactivePropertySlim<T>>
@@ -18,9 +18,13 @@ namespace Takker.DataAccess
 		/// <param name="value">バインド変数にマッピングするプロパティを表すReactivePropertySlim<T>。</param>
 		public override void SetValue(IDbDataParameter parameter, ReactivePropertySlim<T> value)
 		{
-            parameter.DbType = default(T) is long ? DbType.Int64 :
-                default(T) is int ? DbType.Int32 : DbType.String;
-			parameter.Value = value.Value;
+            parameter.DbType = value switch
+            {
+                ReactivePropertySlim<long> _ => DbType.Int64,
+                ReactivePropertySlim<int> _ => DbType.Int32,
+                _ => DbType.String,
+            };
+            parameter.Value = value.Value;
 		}
 	}
 }
